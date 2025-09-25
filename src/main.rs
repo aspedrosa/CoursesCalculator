8766d4c69b93469a26ae7c76d0f08cf200e5207f
aspedrosa <aspedr0sa@protonmail.com>
2025-09-25 21:41:12 +0100

    Download stage zip and load it into a polars dataframe

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::fs::{create_dir_all, File};
 use std::io::Write;
+use std::path::Path;
+use polars::prelude::{CsvParseOptions, CsvReadOptions};
+use polars::prelude::SerReader;
 use reqwest::blocking::Client;
 use scraper::{Html, Selector};
 
@@ -61,10 +64,9 @@ fn get_event_stages(event_id: u32) -> Result<Vec<Stage>, String> {
         .filter_map(|tr| {
             let tds: Vec<_> = tr.select(&td_sel).collect();
             let title = tds.get(0)?.select(&a_sel).next()?.text().collect::<String>().replace('\n', "");
-            let href = tds.get(2)?.select(&a_sel).last()?.attr("href")?;
             Some(Stage {
                 title,
-                file_link: href.to_string(),
+                file_link: tds.get(2)?.select(&a_sel).last()?.attr("href")?.to_string(),
             })
         })
         .collect();
@@ -72,12 +74,73 @@ fn get_event_stages(event_id: u32) -> Result<Vec<Stage>, String> {
     Ok(stages)
 }
 
+fn download_stage_zip(event_id: u32, stage_id: u32, url: &str) -> Result<(), String> {
+    let filepath = format!("data/zip/{}/{}.zip", event_id, stage_id);
+    if Path::exists(filepath.as_ref()) {
+        return Ok(())
+    }
+
+    let client = Client::new();
+    let response = client.get(url).send().unwrap();
+    if !response.status().is_success() {
+        return Err(format!("Failed to download zip: {}", filepath));
+    }
+
+    let content = response.bytes().unwrap();
+    create_dir_all(format!("data/zip/{}", event_id)).expect("Failed to create data directory");
+    let mut file = File::create(&filepath).expect("Failed to create zip file");
+    file.write_all(&content).unwrap();
+    println!("Downloaded: {}", filepath);
+    Ok(())
+
+}
+
+fn extract_stage_zip(event_id: u32, stage_id: u32) {
+    let zip_filepath = format!("data/zip/{}/{}.zip", event_id, stage_id);
+    let extract_path = format!("data/unzipped/{}", event_id);
+
+    let file = File::open(&zip_filepath).expect("Failed to open zip file");
+    let mut archive = zip::ZipArchive::new(file).expect("Failed to read zip archive");
+
+    create_dir_all(&extract_path).expect("Failed to create extract directory");
+
+    let mut file = archive.by_index(0).expect("Failed to access file in zip");
+    let outpath = std::path::Path::new(&extract_path).join(format!("{}.csv", stage_id));
+
+    let mut outfile = File::create(&outpath).expect("Failed to create file in extract path");
+    std::io::copy(&mut file, &mut outfile).expect("Failed to copy file content");
+
+    println!("Extracted: {}", extract_path);
+}
+
+fn calc_best_portuguese_per_class(stage_df: &polars::prelude::DataFrame) {
+}
+
+fn calc_best_per_class(stage_df: &polars::prelude::DataFrame) {
+}
+
 fn main() {
     let event_id = 2994;  // entroncamento city race 2025
 
     let stages = get_event_stages(event_id);
+    if let Ok(stages) = stages {
+        for (stage_id, stage) in stages.iter().enumerate() {
+            download_stage_zip(event_id, stage_id as u32, stage.file_link.as_str()).expect(format!("Failed to download stage zip: {}", stage.title).as_str());
 
-    for stage in stages {
-        println!("{:?}", stage)
+            extract_stage_zip(event_id, stage_id as u32);
+
+            let csv_path = format!("data/unzipped/{}/{}.csv", event_id, stage_id);
+
+            let df_csv = CsvReadOptions::default()
+                .with_has_header(true)
+                .with_parse_options(
+                    CsvParseOptions::default()
+                        .with_separator(b';')
+                        .with_truncate_ragged_lines(true)
+                ).try_into_reader_with_file_path(Some(csv_path.into()))
+                .unwrap().finish().unwrap();
+
+            println!("{}", df_csv);
+        }
     }
 }
