c06bf1f4b646b7feada530188648acb900871848
aspedrosa <aspedr0sa@protonmail.com>
2025-10-02 22:18:03 +0100

    Handle not classified

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,6 +1,7 @@
 mod event_scrape;
 
-use polars::prelude::{CsvParseOptions, CsvReadOptions};
+use std::sync::Arc;
+use polars::prelude::{CsvParseOptions, CsvReadOptions, DataType, Field, Schema, SchemaRef};
 use polars::prelude::SerReader;
 
 #[derive(Debug)]
@@ -19,6 +20,13 @@ fn calc_best_per_class(stage_df: &polars::prelude::DataFrame) {
 pub async fn fetch_event(event_id: u32) {
     let stages = event_scrape::get_event_stages(event_id).await;
     if let Ok(stages) = stages {
+
+        let schema = Schema::from_iter(vec![
+            Field::new("nc".into(), DataType::String),
+            Field::new("Place".into(), DataType::String),  // there can be "nc"
+        ]);
+        let schema = Arc::new(schema);
+
         for (stage_id, stage) in stages.iter().enumerate() {
             event_scrape::download_stage_zip(event_id, stage_id as u32, stage.file_link.as_str()).await.expect(format!("Failed to download stage zip: {}", stage.title).as_str());
 
@@ -28,6 +36,7 @@ pub async fn fetch_event(event_id: u32) {
 
             let df_csv = CsvReadOptions::default()
                 .with_has_header(true)
+                .with_schema_overwrite(Some(schema.clone()))
                 .with_parse_options(
                     CsvParseOptions::default()
                         .with_separator(b';')
